fdf4ec3302e8e641872e17c2045e193ee8cffee8
SVVerbovskiy <verbovskii@inbox.ru>
2023-08-19 18:17:04 +0500

    lint bag fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {  
+module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
